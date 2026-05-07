3fb0dc4bcb42dc477edac1fe148612238b6b0fa0
Nary-Vip <nary2vip@gmail.com>
2026-05-07 14:59:44 +0530

    Package name updated';

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
 
 
 android {
-    namespace = "com.example.on_dev_llm"
+    namespace = "poc.rq.ondevllm"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.on_dev_llm"
+        applicationId = "poc.rq.ondevllm"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
--- android/app/src/main/kotlin/poc/rq/ondevllm/MainActivity.kt
+++ android/app/src/main/kotlin/poc/rq/ondevllm/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.on_dev_llm
+package poc.rq.ondevllm
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
